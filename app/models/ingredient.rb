b202a4f76cc88af33e874c6d92c2f94a3c023f2d
Mai Nguyen <mai.nguyen.gl@gmail.com>
2021-03-05 15:29:19 +0100

    Add dotenv - Protect my secret data in .env file

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+  has_many :coctails, through: :does
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :doses, only: [:new, :create]
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
